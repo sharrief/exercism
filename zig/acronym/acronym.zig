8b705cbf21f883afa2f56404e98985b370d991e5
Sharrief Shabazz <me@sharrief.com>
2025-11-10 19:06:54 -0800

    Submit acronym with ArrayList

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -2,30 +2,21 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
-    const indicies_of_starting_letters = try allocator.alloc(usize, words.len);
-    defer allocator.free(indicies_of_starting_letters);
+    var acronym = std.ArrayList(u8).init(allocator);
 
-    var num_starting_letters: usize = 0;
     var letter_is_start_of_word = true;
 
-    for (words, 0..) |char, idx_of_char| {
+    for (words) |char| {
         if (isLetter(char) and letter_is_start_of_word) {
-            indicies_of_starting_letters[num_starting_letters] = idx_of_char;
-            num_starting_letters += 1;
+            try acronym.append(capitalize(char));
             letter_is_start_of_word = false;
         } else if (isWordSeparator(char)) {
             letter_is_start_of_word = true;
         }
     }
 
-    const acronym = try allocator.alloc(u8, num_starting_letters);
-    var acronym_size: usize = 0;
-    for (0..num_starting_letters) |i| {
-        acronym[acronym_size] = capitalize(words[indicies_of_starting_letters[i]]);
-        acronym_size += 1;
-    }
-    std.debug.print("input: {s}\noutput: {s}\n", .{ words, acronym });
-    return acronym;
+    std.debug.print("input: {s}\noutput: {s}\n", .{ words, acronym.items });
+    return try acronym.toOwnedSlice();
 }
 
 fn isLetter(char: u8) bool {
