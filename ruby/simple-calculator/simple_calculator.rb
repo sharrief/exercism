2183acfb0caa57a0781c1cd8690ceb399d3cd5b0
Sharrief Shabazz <me@sharrief.com>
2025-11-27 11:13:41 -0800

    Save ruby simple calculator

--- ruby/simple-calculator/simple_calculator.rb
+++ ruby/simple-calculator/simple_calculator.rb
@@ -1,7 +1,22 @@
 class SimpleCalculator
-  ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
-
+  class UnsupportedOperation < StandardError
+  end
+  @@ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+    if (!@@ALLOWED_OPERATIONS.include?(operation))
+      raise UnsupportedOperation.new("\"#{operation}\" not in [#{@@ALLOWED_OPERATIONS.join(',')}]") 
+    end
+
+    if(!first_operand.is_a?(Numeric) || !second_operand.is_a?(Numeric)) 
+      raise ArgumentError.new("Both operands must be numbers")
+    end
+
+    if (operation == '/' && second_operand == 0)
+      return "Division by zero is not allowed."
+    end
+
+    exp = "#{first_operand} #{operation} #{second_operand}"
+    ans = eval(exp)
+    exp + " = #{ans}"
   end
 end
