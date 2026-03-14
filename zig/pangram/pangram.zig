8b0bf8fa83a756b1eae9f568fec091c7f21f67c3
Sharrief Shabazz <me@sharrief.com>
2026-03-14 11:18:10 -0700

    New solution for zig pangram

--- zig/pangram/pangram.zig
+++ zig/pangram/pangram.zig
@@ -1,21 +1,37 @@
+const std = @import("std");
+
+const allLetterFlagsEnabled: u26 = 0b11111111111111111111111111;
+const debugging = true;
+
 pub fn isPangram(str: []const u8) bool {
-    var lettersUsed: u256 = 0;
+    var lettersFlaggedInPangram: u26 = 0;
     for (str) |char| {
-        var letter = char;
+        // 2^5 is enough to count the 26 English letters
+        var letterIndexInAlphabet: u5 = undefined;
         if ('A' <= char and char <= 'Z') {
-            // convert upper case letters to lower case
-            letter += 'a' - 'A';
+            letterIndexInAlphabet = @intCast(char - 'A');
+        } else if ('a' <= char and char <= 'z') {
+            letterIndexInAlphabet = @intCast(char - 'a');
+        } else {
+            // skip non-English letter character codes
+            continue;
         }
-        if ('a' <= letter and letter <= 'z') {
-            // is lower case letter
-            // set flag so we know this letter was seen
-            lettersUsed |= (@as(u256, 1) << letter);
-        }
+        debug("Letter {c} index {d}\n", .{ char, letterIndexInAlphabet });
+        // set flag for letter at this index
+        lettersFlaggedInPangram |= @as(u26, 1) << letterIndexInAlphabet;
+        debug("Flags: {b:026}\n", .{lettersFlaggedInPangram});
     }
-    var allLetters: u256 = 0;
-    for ('a'..'z' + 1) |i| {
-        const letter: u8 = @intCast(i);
-        allLetters |= (@as(u256, 1) << letter);
+    return lettersFlaggedInPangram == allLetterFlagsEnabled;
+}
+
+fn debug(comptime fmt: []const u8, args: anytype) void {
+    if (!debugging) {
+        return;
     }
-    return lettersUsed == allLetters; 
+    std.debug.print(fmt, args);
+}
+
+pub fn main() void {
+    const result = isPangram("The quick brown fox jumps over the lazy dog.");
+    debug("isPangram {}", .{result});
 }
