6335ee83c328e3ca1718eefd311fd24372ab5290
Sharrief Shabazz <me@sharrief.com>
2026-03-16 09:11:20 -0700

    Update zig phone_number

--- zig/phone-number/phone_number.zig
+++ zig/phone-number/phone_number.zig
@@ -12,31 +12,32 @@
 // * Memory is a resource.
 // * Together we serve the users.
 
-const std = @import("std");
-
+/// Extracts a North American phone number from the phrase.
+/// Returns null if the digits in the input don't make a valid phone number.
+/// Ignores non-digits in the input.
 pub fn clean(phrase: []const u8) ?[10]u8 {
     var phone_number = [_]u8{0} ** 10;
-    var i: usize = 0;
+    var digit_index: usize = 0; // indexes into phone_number
     for (phrase) |char| {
         if (char < '0' or '9' < char) {
-            // ignore non-numeric characters
+            // ignore non-digits
             continue;
-        } else if (i >= 10) {
-            // phone number is invalid if more than 10 non-numeric numbers
+        } else if (digit_index >= 10) {
+            // phrase is not a phone number if it has more than 10 digits (excl opt country code)
             return null;
-        } else if (i == 0 and char == '1') {
+        } else if (digit_index == 0 and char == '1') {
             // ignore the leading NA country code
             continue;
-        } else if ((i == 0 or i == 3) and (char == '0' or char == '1')) {
+        } else if ((digit_index == 0 or digit_index == 3) and (char == '0' or char == '1')) {
             // 0 and 1 are not allowed at indexes 0 and 3 for NA phone numbers
             return null;
         } else {
-            phone_number[i] = char;
-            i += 1;
+            phone_number[digit_index] = char;
+            digit_index += 1;
         }
     }
-    if (i != 10) {
-        // phone number is invalid if less than 10 non-numeric numbers
+    if (digit_index != 10) {
+        // phone number is invalid if not exactly 10 digits
         return null;
     }
     return phone_number;
